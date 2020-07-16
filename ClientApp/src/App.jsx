4907a76c526a2a99b2f7c75323f76b81c0bfdd2f
Kento Kawakami <kawakamikento@gmail.com>
2020-07-16 17:34:28 -0400

    Moved graphics folder again

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './custom.scss'
+import logo from '. /graphics/logo/logo-on-black.png'
 
 export function App() {
   return (
     <>
       <nav>
         <div>
-          {/* <img src={} /> */}
+          <img src={logo} />
           <div>
             <button>Sign up</button>
             <button>Log in</button>
