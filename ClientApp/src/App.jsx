0086cc3fd5a2c44c7e5f64f978a74190223bf1fb
Kento Kawakami <kawakamikento@gmail.com>
2020-07-16 18:22:59 -0400

    added browser sync on phone

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './custom.scss'
-import logo from '. /graphics/logo/logo-on-black.png'
+import './typeface.css'
+import logo from './graphics/logo/logo-on-black.png'
 
 export function App() {
   return (
     <>
       <nav>
         <div>
-          <img src={logo} />
-          <div>
+          <img src={logo} alt="Smash combos logo" />
+
+          <div className="nav-buttons">
             <button>Sign up</button>
             <button>Log in</button>
           </div>
--- Startup.cs
+++ Startup.cs
@@ -47,12 +47,11 @@ namespace Smash_Combos
             }
             else
             {
+                app.UseHttpsRedirection();
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseSwagger();
