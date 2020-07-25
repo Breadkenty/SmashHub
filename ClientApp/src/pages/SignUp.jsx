59067916458f4b2a3c1630f47501f7023f66c5d4
Kento Kawakami <kawakamikento@gmail.com>
2020-07-25 15:28:04 -0400

    Added feature to sign up users

--- ClientApp/src/pages/SignUp.jsx
+++ ClientApp/src/pages/SignUp.jsx
@@ -1,13 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function SignUp() {
+  const [newUser, setNewUser] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+  })
+
+  const [errorMessage, setErrorMessage] = useState()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    fetch('api/Users', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(newUser),
+    })
+      .then(response => response.json())
+      .then(apiResponse => {
+        if (apiResponse.status === 400) {
+          setErrorMessage(Object.values(apiResponse.errors).join(' '))
+        } else {
+          history.pushState('/')
+        }
+      })
+  }
+
+  function handleFieldChange(event) {
+    setNewUser({
+      ...newUser,
+      [event.target.id]: event.target.value,
+    })
+  }
+
   return (
     <div className="login-signup">
       <h3 className="text-white">Sign up</h3>
-      <form>
-        <input className="bg-yellow" type="text" placeholder="Display name" />
-        <input className="bg-yellow" type="text" placeholder="Email" />
-        <input className="bg-yellow" type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <h3>{errorMessage}</h3>
+        <input
+          className="bg-yellow"
+          type="text"
+          placeholder="Display name"
+          id="displayName"
+          value={newUser.displayName}
+          onChange={handleFieldChange}
+        />
+        <input
+          className="bg-yellow"
+          type="text"
+          placeholder="Email"
+          id="email"
+          value={newUser.email}
+          onChange={handleFieldChange}
+        />
+        <input
+          className="bg-yellow"
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={newUser.password}
+          onChange={handleFieldChange}
+        />
         <button className="bg-yellow button black-text" type="submit">
           Sign up
         </button>
