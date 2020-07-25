4e34f87f695169b94b26251318af94fe55460b12
Kento Kawakami <kawakamikento@gmail.com>
2020-07-25 18:04:39 -0400

    Added the feature to log in and out

--- ClientApp/src/components/SideNav.jsx
+++ ClientApp/src/components/SideNav.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../graphics/logo/logo-on-black.png'
 import useOnClickOutside from 'use-onclickoutside'
+import { isLoggedIn, logout } from '../auth'
 
 export function SideNav(props) {
   const ref = React.useRef(null)
@@ -11,6 +12,11 @@ export function SideNav(props) {
       props.handleSideBar()
     }
   })
+
+  function handleLogout() {
+    logout()
+    window.location = '/'
+  }
 
   return (
     <div
@@ -31,56 +37,75 @@ export function SideNav(props) {
         <img src={logo} alt="Smash combos logo" onClick={props.handleSideBar} />
       </Link>
       <ul>
-        <li>
-          <Link to="/submit">
-            <button
-              className="bg-yellow button black-text"
-              onClick={props.handleSideBar}
-            >
-              Submit a combo
+        {isLoggedIn() && (
+          <li>
+            <Link to="/submit">
+              <button
+                className="bg-yellow button black-text"
+                onClick={props.handleSideBar}
+              >
+                Submit a combo
+              </button>
+            </Link>
+          </li>
+        )}
+
+        {isLoggedIn() && (
+          <>
+            <li>
+              <Link to="/add">
+                <button
+                  className="bg-yellow button black-text"
+                  onClick={props.handleSideBar}
+                >
+                  Add a Character
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/edit">
+                <button
+                  className="bg-yellow button black-text"
+                  onClick={props.handleSideBar}
+                >
+                  Edit a Character
+                </button>
+              </Link>
+            </li>
+          </>
+        )}
+
+        {isLoggedIn() || (
+          <li>
+            <Link to="/signup">
+              <button
+                className="white-text button-blank"
+                onClick={props.handleSideBar}
+              >
+                Sign up
+              </button>
+            </Link>
+          </li>
+        )}
+        {isLoggedIn() || (
+          <li>
+            <Link to="/login">
+              <button
+                className="white-text button-blank"
+                onClick={props.handleSideBar}
+              >
+                Log in
+              </button>
+            </Link>
+          </li>
+        )}
+        {isLoggedIn() && (
+          <li>
+            <button className="white-text button-blank" onClick={handleLogout}>
+              Log out
             </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/add">
-            <button
-              className="bg-yellow button black-text"
-              onClick={props.handleSideBar}
-            >
-              Add a Character
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/edit">
-            <button
-              className="bg-yellow button black-text"
-              onClick={props.handleSideBar}
-            >
-              Edit a Character
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/signup">
-            <button
-              className="white-text button-blank"
-              onClick={props.handleSideBar}
-            >
-              Sign up
-            </button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/login">
-            <button
-              className="white-text button-blank"
-              onClick={props.handleSideBar}
-            >
-              Log in
-            </button>
-          </Link>
-        </li>
+          </li>
+        )}
       </ul>
     </div>
   )
--- ClientApp/src/pages/LogIn.jsx
+++ ClientApp/src/pages/LogIn.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { recordAuthentication } from '../auth'
+import { useHistory } from 'react-router'
 
 export function LogIn() {
+  const history = useHistory()
+
+  const [errorMessage, setErrorMessage] = useState()
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  function handleFieldChange(event) {
+    setUser({
+      ...user,
+      [event.target.id]: event.target.value,
+    })
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    fetch('api/Sessions', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user),
+    })
+      .then(response => response.json())
+      .then(apiResponse => {
+        if (apiResponse.status === 400) {
+          setErrorMessage(Object.values(apiResponse.errors).join(' '))
+        } else {
+          recordAuthentication(apiResponse)
+          window.location = '/'
+        }
+      })
+  }
+
   return (
     <div className="login-signup">
       <h3 className="white-text">Log in</h3>
-      <form>
-        <input className="bg-yellow" type="text" placeholder="Email" />
-        <input className="bg-yellow" type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="bg-yellow"
+          type="text"
+          placeholder="Email"
+          id="email"
+          value={user.email}
+          onChange={handleFieldChange}
+        />
+        <input
+          className="bg-yellow"
+          type="password"
+          placeholder="Password"
+          id="password"
+          value={user.password}
+          onChange={handleFieldChange}
+        />
         <button className="bg-yellow button black-text" type="submit">
           Log in
         </button>
