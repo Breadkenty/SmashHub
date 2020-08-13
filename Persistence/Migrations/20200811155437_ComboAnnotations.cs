00badbe108ee7571108d57f004c4645835582c97
Hayden Carson <info@haydencarson.com.au>
2020-08-13 23:24:44 +1000

    Create Domain and Persistence projects initial building commit

---
+++ Persistence/Migrations/20200811155437_ComboAnnotations.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Smash_Combos.Persistence.Migrations
+{
+    public partial class ComboAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Combos",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Combos",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Combos",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Notes",
+                table: "Combos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 512,
+                oldNullable: true);
+        }
+    }
+}
