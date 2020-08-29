cd2b910c2d67d612de4019855be157f082a1a65e
sleeping-dev <maximilian.tomio@students.fhv.at>
2020-08-29 22:26:01 +0200

    💾Added ResponseBase and ResponseStatus to make Cqrs-Responses easier readable [experimental]

---
+++ Smash_Combos.Core/Cqrs/ResponseBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Smash_Combos.Core.Cqrs
+{
+    public abstract class ResponseBase
+    {
+        public ResponseStatus Status { get; set; }
+    }
+
+    public abstract class ResponseBase<T> : ResponseBase
+    {
+        public T Data { get; set; }
+    }
+}
