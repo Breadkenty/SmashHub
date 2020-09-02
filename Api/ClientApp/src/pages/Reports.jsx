911bc2677bdab6e9e0c32787b52e981422aea9dc
Kento Kawakami <kawakamikento@gmail.com>
2020-09-02 00:22:18 -0400

    💜 Plugged the Reports page to the API

--- Api/ClientApp/src/pages/Reports.jsx
+++ Api/ClientApp/src/pages/Reports.jsx
@@ -1,7 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 export function Reports() {
+  const [users, setUsers] = useState([])
+
+  function getReports() {
+    fetch('/api/Users')
+      .then(response => response.json())
+      .then(apiData => {
+        setUsers(apiData)
+      })
+  }
+
+  function sumReports(user) {
+    let totalReports = 0
+    user.combos.forEach(
+      combo => (totalReports = totalReports + combo.reports.length)
+    )
+    user.comments.forEach(
+      comment => (totalReports = totalReports + comment.reports.length)
+    )
+    return totalReports
+  }
+
+  useEffect(getReports, [])
+  console.log(users)
+
   return (
     <div className="reports">
       <header>
@@ -11,8 +35,14 @@ export function Reports() {
       <section>
         <p>Reports</p>
         <p>User</p>
-        <p>2</p>
-        <Link to="/Breadkenty">BreadKenty</Link>
+        {users
+          .filter(user => sumReports(user) > 0)
+          .map(user => (
+            <>
+              <p>{sumReports(user)}</p>
+              <Link to={`/${user.displayName}`}>{user.displayName}</Link>
+            </>
+          ))}
       </section>
     </div>
   )
