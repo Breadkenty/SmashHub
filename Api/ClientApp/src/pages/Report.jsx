8f0c8e3dd28cb5dfdbfcaf3d2883bc2547f29fb4
Kento Kawakami <kawakamikento@gmail.com>
2020-09-03 23:48:11 -0400

    💜Optimized report content in the reports page to link both to the comment and combo page

--- Api/ClientApp/src/pages/Report.jsx
+++ Api/ClientApp/src/pages/Report.jsx
@@ -1,22 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 
 export function Report(props) {
   const [confirmDismiss, setConfirmDismiss] = useState(false)
+  const [report, setReport] = useState({
+    body: '',
+    combo: {
+      id: {},
+      character: {
+        variableName: 'hi',
+      },
+      title: '',
+    },
+    comment: {
+      combo: {
+        id: 0,
+        character: {
+          variableName: 'hi',
+        },
+      },
+      body: '',
+    },
+    dateReported: '',
+    dismiss: false,
+    id: 0,
+    reporter: {},
+    user: {},
+  })
 
   const handleDismiss = event => {
     event.preventDefault()
     console.log('Dismiss:')
   }
 
+  useEffect(() => {
+    setReport(props.report)
+  }, [])
+
   return (
     <div className="reports-row">
       <p>{moment(props.report.dateReported).format('L')}</p>
-      <Link to="#">Down tilt ground float nair</Link>
-      <p>{props.report.body}</p>
-      <Link to={`/user/${props.report.reporter.displayName}`}>
-        {props.report.reporter.displayName}
+      <Link
+        to={
+          report.combo
+            ? `/character/${report.combo.character.variableName}/${report.combo.id}`
+            : `/character/${report.comment.combo.character.variableName}/${report.comment.combo.id}`
+        }
+      >
+        {report.combo
+          ? `Combo: ${report.combo.title}`
+          : `Comment: ${report.comment.body}`}
+      </Link>
+      <p>{report.body}</p>
+      <Link to={`/user/${report.reporter.displayName}`}>
+        {report.reporter.displayName}
       </Link>
       {confirmDismiss ? (
         <div className="report-dismiss">
