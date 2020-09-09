acfb42653c77413911019cc00e835a0d78e7ba51
sleeping-dev <maximilian.tomio@students.fhv.at>
2020-09-09 13:55:27 +0200

    🔥Removed unused statement in Startup

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -85,7 +85,6 @@ namespace Smash_Combos
             else
             {
                 app.UseHttpsRedirection();
-                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
