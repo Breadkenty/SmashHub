ceb70f981486b9acf5f82c8d9352c2e0558aef38
Kento Kawakami <kawakamikento@gmail.com>
2020-09-09 02:25:49 -0400

    🎨Updated announcements

--- Api/ClientApp/src/pages/Characters.jsx
+++ Api/ClientApp/src/pages/Characters.jsx
@@ -35,26 +35,17 @@ export function Characters() {
         </div>
       </header>
       <section className="announcements">
-        <h4>Announcements</h4>
+        <h3>Announcements</h3>
         <ul>
-          <li className="announcement">
-            <h5>
-              To view the demo of how this app works, feel free to watch my
-              presentation!
-            </h5>
-            <a href="https://youtu.be/7lSaNBIl1Y8?t=3516">
-              The Demo (skip to 1:02:05 to skip the introduction)
-            </a>
+          <li>
+            <h4>We're back!</h4>
+            <p>
+              We're happy to announce we've implemented a moderator feature to
+              keep the contents of Smash Combos appropriate. Please check the
+              rules before posting!
+            </p>
           </li>
           <li>
-            <h5>
-              This project is a work in progress, please submit feature requests
-              by tweeting to
-            </h5>
-            <a href="https://twitter.com/KentoKawakami/">@KentoKawakami</a>
-          </li>
-          <li>
-            <h5>Or join our official discord server</h5>
             <a href="https://discord.gg/VbnAwUg">
               Official Smash Combos Discord
             </a>
