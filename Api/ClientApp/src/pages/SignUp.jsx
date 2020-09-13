20d6ad19edc1d947d7662ffc11cc4e0ccda046eb
Kento Kawakami <kawakamikento@gmail.com>
2020-09-13 13:42:57 -0400

    🎨Added confirm password on sign up

--- Api/ClientApp/src/pages/SignUp.jsx
+++ Api/ClientApp/src/pages/SignUp.jsx
@@ -9,37 +9,42 @@ export function SignUp() {
   })
 
   const [errorMessage, setErrorMessage] = useState()
+  const [confirmPassword, setConfirmPassword] = useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
 
-    fetch('api/Users', {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(newUser),
-    })
-      .then(response => {
-        if (response.status === 200) {
-          fetch('api/Sessions', {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(newUser),
-          })
-            .then(response => response.json())
-            .then(apiResponse => {
-              recordAuthentication(apiResponse)
-              window.location = '/'
+    if (`${newUser.password}` === `${confirmPassword}`) {
+      fetch('api/Users', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newUser),
+      })
+        .then(response => {
+          if (response.status === 200) {
+            fetch('api/Sessions', {
+              method: 'POST',
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify(newUser),
             })
-          return { then: function() {} }
-        } else {
-          return response.json()
-        }
-      })
-      .then(apiData => {
-        setErrorMessage(
-          apiData.detail || Object.values(apiData.errors).join(' ')
-        )
-      })
+              .then(response => response.json())
+              .then(apiResponse => {
+                recordAuthentication(apiResponse)
+                window.location = '/'
+              })
+            return { then: function() {} }
+          } else {
+            return response.json()
+          }
+        })
+        .then(apiData => {
+          setErrorMessage(
+            apiData.detail || Object.values(apiData.errors).join(' ')
+          )
+        })
+    } else {
+      setErrorMessage('Confirm password does not match')
+    }
   }
 
   function handleFieldChange(event) {
@@ -81,6 +86,15 @@ export function SignUp() {
           id="password"
           value={newUser.password}
           onChange={handleFieldChange}
+        />
+        <input
+          className="bg-yellow"
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={event => {
+            setConfirmPassword(event.target.value)
+          }}
         />
         <button className="bg-yellow button black-text" type="submit">
           Sign up
