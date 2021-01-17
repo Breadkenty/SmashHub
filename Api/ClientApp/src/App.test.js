7a8576216bdbf90daf9ebc958afc98a6f9c938c8
Kento Kawakami <kawakamikento@gmail.com>
2021-01-17 02:41:43 -0500

    📦 Fixed set up for jest testing

--- Api/ClientApp/src/App.test.js
+++ Api/ClientApp/src/App.test.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
 
-it('renders without crashing', async () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>, div);
-  await new Promise(resolve => setTimeout(resolve, 1000));
-});
+// test('renders learn react link', () => {
+//   render(<App />)
+//   const linkElement = screen.getByText(/kento kawakami/i)
+//   expect(linkElement).toBeInTheDocument()
+// })
