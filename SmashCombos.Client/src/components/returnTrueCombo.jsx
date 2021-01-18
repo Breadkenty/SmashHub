f2669191b36872e08f9416ac05755c21f12e8dd4
Kento Kawakami <kawakamikento@gmail.com>
2021-01-17 20:55:19 -0500

    🔥 Deleted Reactapp

--- SmashCombos.Client/src/components/returnTrueCombo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export function returnTrueCombo(trueCombo) {
-  if (trueCombo) {
-    return <div className="true-combo">true</div>
-  } else {
-    return <div className="not-true-combo">not true</div>
-  }
-}
