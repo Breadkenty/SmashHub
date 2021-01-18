bb754719b3c9779ac722b109c3e0290f0bd1eba3
Kento Kawakami <kawakamikento@gmail.com>
2021-01-17 20:55:19 -0500

    🔥 Deleted Reactapp

--- SmashCombos.Client/src/components/sortingFunctions.jsx
+++
@@ -1,5 +0,0 @@
-export const sortingFunctions = {
-  best: (a, b) => b.netVote - a.netVote,
-  newest: (a, b) => new Date(b.datePosted) - new Date(a.datePosted),
-  oldest: (a, b) => new Date(a.datePosted) - new Date(b.datePosted),
-}
