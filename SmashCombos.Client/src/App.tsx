62463b776ea84863316ae7f71318bd03c16a60e5
Kento Kawakami <kawakamikento@gmail.com>
2021-01-24 14:38:02 -0500

    Added stuff lol

--- SmashCombos.Client/src/App.tsx
+++ SmashCombos.Client/src/App.tsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Huh?</h1>
+      <h1>Huh?</h1>
+      <h1>Huh?</h1>
+      <h1>Huh?</h1>
+      <h1>Huh?</h1>
+      <h1>Huh?</h1>
+      <h1>Huh?</h1>
     </div>
   );
 }
