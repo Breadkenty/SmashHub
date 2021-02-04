85826880cf0622a3f56fc70bbecf2f97446007ff
Kento Kawakami <kawakamikento@gmail.com>
2021-02-03 23:25:46 -0500

    Added .env variables

--- SmashCombos.Client/src/App.tsx
+++ SmashCombos.Client/src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const environment = process.env.NODE_ENV;
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
+  const [characters, setCharacters] = useState([]);
+
+  function getCharacters() {
+    fetch(`${baseUrl}/characters`)
+      .then((response) => response.json())
+      .then((apiData) => setCharacters(apiData));
+  }
+
+  getCharacters();
+
+  console.log(characters);
   return (
     <div className="App">
       <h1>Huh? what did you say?</h1>
-      {environment}
     </div>
   );
 }
