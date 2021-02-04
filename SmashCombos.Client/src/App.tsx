635c9e35d606b67c1056adc24374649e6317c1eb
Kento Kawakami <kawakamikento@gmail.com>
2021-02-03 20:29:56 -0500

    Minor change to test environment

--- SmashCombos.Client/src/App.tsx
+++ SmashCombos.Client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function App() {
+  const environment = process.env.NODE_ENV;
   return (
     <div className="App">
       <h1>Huh? what did you say?</h1>
+      {environment}
     </div>
   );
 }
