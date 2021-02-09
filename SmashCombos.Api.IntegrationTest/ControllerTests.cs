dff03e25a2594c4948813bc78c514d06c0abd755
sleeping-dev <maximilian.tomio@students.fhv.at>
2021-02-09 14:21:12 +0100

    Implemented basic GET-Tests for Characters and Combos

---
+++ SmashCombos.Api.IntegrationTest/ControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace SmashCombos.Api.Tests.Integration
+{
+    public class ControllerTests
+    {
+        private APIWebApplicationFactory<Startup> _factory;
+        protected HttpClient Client;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _factory = new APIWebApplicationFactory<Startup>();
+            Client = _factory.CreateClient();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            Client.Dispose();
+            _factory.CloseDbConnection();
+            _factory.Dispose();
+        }
+    }
+}
