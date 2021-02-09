ca4d368e573d20e30664fea59bdbe8d7fefc7a62
sleeping-dev <maximilian.tomio@students.fhv.at>
2021-02-09 14:24:02 +0100

    Removed unnecessary verbose comments from CombosController

--- SmashCombos.Api/Controllers/CombosController.cs
+++ SmashCombos.Api/Controllers/CombosController.cs
@@ -21,9 +21,6 @@ using System.Threading.Tasks;
 
 namespace SmashCombos.Controllers
 {
-    // All of these routes will be at the base URL:     /api/Combos
-    // That is what "api/[controller]" means below. It uses the name of the controller
-    // in this case CombosController to determine the URL
     [Route("[controller]")]
     [ApiController]
     public class CombosController : ControllerBase
@@ -35,33 +32,12 @@ namespace SmashCombos.Controllers
             _mediator = mediator;
         }
 
-        // GET: api/Combos
-        //
-        // Returns a list of all your Combos
-        //
         [HttpGet]
         public async Task<ActionResult<IEnumerable<GetCombosResponse>>> GetCombos() => Ok(await _mediator.Send(new GetCombosRequest()));
 
-        // GET: api/Combos/5
-        //
-        // Fetches and returns a specific combo by finding it by id. The id is specified in the
-        // URL. In the sample URL above it is the `5`.  The "{id}" in the [HttpGet("{id}")] is what tells dotnet
-        // to grab the id from the URL. It is then made available to us as the `id` argument to the method.
-        //
         [HttpGet("{id}")]
         public async Task<ActionResult<GetComboResponse>> GetCombo([FromRoute] int id) => Ok(await _mediator.Send(new GetComboRequest { ComboId = id }));
 
-        // PUT: api/Combos/5
-        //
-        // Update an individual combo with the requested id. The id is specified in the URL
-        // In the sample URL above it is the `5`. The "{id} in the [HttpPut("{id}")] is what tells dotnet
-        // to grab the id from the URL. It is then made available to us as the `id` argument to the method.
-        //
-        // In addition the `body` of the request is parsed and then made available to us as a Combo
-        // variable named combo. The controller matches the keys of the JSON object the client
-        // supplies to the names of the attributes of our Combo POCO class. This represents the
-        // new values for the record.
-        //
         [HttpPut("{id}")]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> PutCombo([FromRoute] int id, [FromBody] PutComboRequest request)
@@ -79,15 +55,6 @@ namespace SmashCombos.Controllers
                 return StatusCode(500);
         }
 
-        // POST: api/Combos
-        //
-        // Creates a new combo in the database.
-        //
-        // The `body` of the request is parsed and then made available to us as a Combo
-        // variable named combo. The controller matches the keys of the JSON object the client
-        // supplies to the names of the attributes of our Combo POCO class. This represents the
-        // new values for the record.
-        //
         [HttpPost]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<ActionResult<PostComboResponse>> PostCombo([FromBody] PostComboRequest request)
@@ -102,12 +69,6 @@ namespace SmashCombos.Controllers
                 return StatusCode(500);
         }
 
-        // DELETE: api/Combos/5
-        //
-        // Deletes an individual combo with the requested id. The id is specified in the URL
-        // In the sample URL above it is the `5`. The "{id} in the [HttpDelete("{id}")] is what tells dotnet
-        // to grab the id from the URL. It is then made available to us as the `id` argument to the method.
-        //
         [HttpDelete("{id}")]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> DeleteCombo([FromRoute] int id)
