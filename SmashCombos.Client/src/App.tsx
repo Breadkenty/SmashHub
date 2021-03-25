d20eb82ae68ca08da8a0df7bd9d2087abad5d20b
Kento Kawakami <kawakamikento@gmail.com>
2021-03-24 20:17:53 -0400

    added Material UI

--- SmashCombos.Client/src/App.tsx
+++ SmashCombos.Client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
 
-const baseUrl = process.env.REACT_APP_API_BASE_URL;
+const baseUrl = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 interface Character {
   id: number;
@@ -22,10 +23,26 @@ function App() {
 
   return (
     <div className="App">
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+
       <h1>Huh? what did you say?</h1>
 
       {characters &&
-        characters.map((character: Character) => <p>{character.name}</p>)}
+        characters.map((character) => (
+          <p key={character.name}>{character.name}</p>
+        ))}
     </div>
   );
 }
--- SmashCombos.Client/src/index.tsx
+++ SmashCombos.Client/src/index.tsx
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import yellow from "@material-ui/core/colors/yellow";
+import grey from "@material-ui/core/colors/grey";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: yellow,
+    secondary: grey,
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
