e6629031fee1356895b9e733ddfb19a768ec5a34
Kento Kawakami <kawakamikento@gmail.com>
2021-06-06 20:52:29 -0400

    build: create frontend apps creates project for component library creates project for web app

---
+++ SmashHub.Client/smashhub-component-library/src/TestComponent/TestComponent.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TestComponent from "./TestComponent";
+
+export default {
+  title: "TestComponent"
+};
+
+export const Primary = () => <TestComponent theme="primary" />;
+
+export const Secondary = () => <TestComponent theme="secondary" />;
---
+++ SmashHub.Client/smashhub-component-library/src/TestComponent/TestComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { TestComponentProps } from "./TestComponent.types";
+
+import "./TestComponent.scss";
+
+const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
+  <div
+    data-testid="test-component"
+    className={`test-component test-component-${theme}`}
+  >
+    <h1 className="heading">I'm the test component</h1>
+    <h2>Made with love by Harvey</h2>
+  </div>
+);
+
+export default TestComponent;
