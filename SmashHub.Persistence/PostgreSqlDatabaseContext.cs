74489612e1ba66cfbcf1ab27310fb29f61e0f879
Kento Kawakami <kawakamikento@gmail.com>
2021-06-06 21:31:50 -0400

    fix: ssl error with database connection strings

--- SmashHub.Persistence/PostgreSqlDatabaseContext.cs
+++ SmashHub.Persistence/PostgreSqlDatabaseContext.cs
@@ -48,16 +48,7 @@ namespace SmashHub.Persistence
             var _connection = connection.Replace("postgres://", String.Empty);
             var connectionParts = Regex.Split(_connection, ":|@|/");
 
-            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder
-            {
-                SslMode = SslMode.Require,
-                Username = connectionParts[0],
-                Password = connectionParts[1],
-                Port = int.Parse(connectionParts[3]),
-                Host = connectionParts[2]
-            };
-
-            return builder.ConnectionString;
+            return $"server={connectionParts[2]};SSL Mode=Require;Trust Server Certificate=true;database={connectionParts[4]}; User Id = { connectionParts[0] }; password={connectionParts[1]}; port={connectionParts[3]}";
         }
     }
 }
