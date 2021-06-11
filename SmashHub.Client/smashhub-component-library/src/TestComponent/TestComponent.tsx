8ed61b5213c95f3ce7d45354e4ecd5aa73ad3649
Kento Kawakami <kawakamikento@gmail.com>
2021-06-10 23:37:35 -0400

    ci: update pipeline to install component library locally

--- SmashHub.Client/smashhub-component-library/src/TestComponent/TestComponent.tsx
+++ SmashHub.Client/smashhub-component-library/src/TestComponent/TestComponent.tsx
@@ -9,7 +9,7 @@ const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
     data-testid="test-component"
     className={`test-component test-component-${theme}`}
   >
-    <h1 className="heading">I'm the test component</h1>
+    <h1 className="heading">I'm the test component?????</h1>
     <h2>Made with love by Harvey</h2>
   </div>
 );
